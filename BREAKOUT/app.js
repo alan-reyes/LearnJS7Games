787749a2835c5b1ea2500033e49e1bd05dcfb281
Alan Reyes <alanr01rh@gmail.com>
2022-08-28 11:46:50 -0500

    add js main game functions

--- BREAKOUT/app.js
+++ BREAKOUT/app.js
@@ -1,6 +1,11 @@
 const grid = document.querySelector('.grid');
 const blockWidth = 100;
 const blockHeight = 20;
+const boardWidth = 560;
+const userStart = [230, 10];
+const ballStart = [270, 40];
+let ballCurrentPosition = ballStart;
+let currentPosition = userStart;
 
 // create block
 class Block {
@@ -44,3 +49,53 @@ function addBlocks() {
 }
 
 addBlocks()
+
+// add user
+const user =  document.createElement('div');
+user.classList.add('user');
+drawUser();
+grid.appendChild(user);
+
+// draw user
+function drawUser() {
+    user.style.left = currentPosition[0] + 'px';
+    user.style.bottom = currentPosition[1] + 'px';
+}
+
+// draw ball
+function drawBall() {
+    ball.style.left = ballCurrentPosition[0] + 'px';
+    ball.style.bottom = ballCurrentPosition[1] + 'px';
+}
+
+// move user
+function moveUser(e) {
+    switch(e.key) {
+        case 'ArrowLeft':
+            if (currentPosition[0] > 0) {
+                currentPosition[0] -= 10;
+                drawUser();    
+            }
+            break;
+        case 'ArrowRight': 
+            if (currentPosition[0] < boardWidth - blockWidth) {
+                currentPosition[0] += 10;
+                drawUser();
+            }
+            break;
+    }
+}
+
+document.addEventListener('keydown', moveUser);
+
+// add ball
+const ball = document.createElement('div');
+ball.classList.add('ball');
+drawBall();
+grid.appendChild(ball);
+
+
+
+
+
+
